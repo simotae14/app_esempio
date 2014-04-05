ccc68f270cfc8dc723c623e96290b9a1f60c5a29
startae14 <startae14@gmail.com>
2014-04-05 11:48:27 +0200

    Fatto con le pagine statiche

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   def home
+  	# var che rappresenta titolo pagina
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
+  end
+
+  def about
+  	@title = "About"
   end
 
 end
